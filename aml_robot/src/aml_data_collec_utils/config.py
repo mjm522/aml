c65b066bc9778e63d0b9e5436751de401af37227
Ermano Arruda <ermano.arruda@gmail.com>
2017-01-08 15:39:39 +0000

    updating config data collection

--- aml_robot/src/aml_data_collec_utils/config.py
+++ aml_robot/src/aml_data_collec_utils/config.py
@@ -22,8 +22,9 @@ TABLE = {
 }
 
 config = {
-    'reset_spot_offset': [-0.10,0,0],
-    'end_effector_tip_offset': [0.0, 0.0, 0.16090282],
-    'pre_push_offsets': [0.20, 0.09, 0.20],
+    'reset_spot_offset': [-0.10,0,0], # w.r.t to box frame
+    'end_effector_tip_offset': [0.0, 0.0, 0.16090282], # w.r.t to end-effector
+    'pre_push_offsets': [0.20, 0.09, 0.20], # w.r.t to box frame
+    'pre_reset_offsets': [0.0, 0.25, 0.0], # w.r.t to box frame
     'box_type': BOX_TYPE_1,
 }
