8271a39b78c949b9a44568ae900fc3a857be8f71
mjm522 <mjm522@cs.bham.ac.uk>
2017-01-15 00:11:31 +0000

    mujoco simulator tests

--- aml_robot/src/aml_lfd/tests/test_inverse_dyn.py
+++ aml_robot/src/aml_lfd/tests/test_inverse_dyn.py
@@ -2,7 +2,7 @@ import rospy
 import numpy as np
 import matplotlib.pyplot as plt
 
-from aml_lfd.utilities.utilities import js_inverse_dynamics, get_sampling_rate
+from aml_lfd.utilities.utilities import js_inverse_dynamics, get_js_traj, get_sampling_rate, get_effort_sequence_of_demo
 
 
 def plot_id(torques):
@@ -23,15 +23,53 @@ def main(robot_interface, load_from_demo, demo_idx):
 
     torques    = js_inverse_dynamics(limb_name=robot_interface._limb, demo_idx=demo_idx, h_component=False)
 
+    js_pos_traj, js_vel_traj, js_acc_traj = get_js_traj(limb_name=robot_interface._limb, demo_idx=demo_idx)
+    
+    effort_sequence = get_effort_sequence_of_demo(limb_name=robot_interface._limb, demo_idx=demo_idx)
+
     sampling_rate = get_sampling_rate(limb_name=robot_interface._limb, demo_idx=demo_idx)
 
-    plot_id(torques)
+    # plot_id(torques)
 
     rate = rospy.Rate(sampling_rate)
+
+    arm.untuck_arm()
+
+    print "Checking computed position commands ..."
+    raw_input('Press any key to continue ...')
+
+    for js_pos in js_pos_traj:
+
+        robot_interface.exec_position_cmd(js_pos)
+
+        rate.sleep()
+
+    raw_input('Completed,  proceeding to torque control, press any key...')
+
+    arm.untuck_arm()
+
+    print "Checking computed torque commands ..."
+    raw_input('Press any key to continue ...')
 
     for tau in torques:
 
         robot_interface.exec_torque_cmd(tau)
+
+        rate.sleep()
+
+    #to reload the position controllers, else the arms will drift
+    robot_interface.exec_position_cmd_delta(np.zeros(7))
+
+    raw_input('Completed,  proceeding to effort control, press any key...')
+
+    arm.untuck_arm()
+
+    print "Checking computed effort commands ..."
+    raw_input('Press any key to continue ...')
+
+    for tau in effort_sequence:
+
+        robot_interface.exec_torque_cmd(-tau)
 
         rate.sleep()
 
@@ -47,8 +85,6 @@ if __name__ == '__main__':
     limb = 'left'
     
     arm = BaxterArm(limb)
-
-    arm.untuck_arm()
 
     demo_idx = 0
 
