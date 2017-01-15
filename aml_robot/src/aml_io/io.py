260f1a4761fd951c122a294498232a7418e5dcae
mjm522 <mjm522@cs.bham.ac.uk>
2017-01-15 22:20:43 +0000

    tests for box2d pushing

--- aml_robot/src/aml_io/io.py
+++ aml_robot/src/aml_io/io.py
@@ -1,31 +1,50 @@
 
 import pickle
+import tensorflow as tf
 
 
 
 
 def save_data(data, filename):
 
-	output = open(filename, 'wb')
+    output = open(filename, 'wb')
 
-	# Pickle dictionary using protocol 0.
-	pickle.dump(data, output)
+    # Pickle dictionary using protocol 0.
+    pickle.dump(data, output)
 
-	# Pickle the list using the highest protocol available.
-	# pickle.dump(selfref_list, output, -1)
+    # Pickle the list using the highest protocol available.
+    # pickle.dump(selfref_list, output, -1)
 
-	output.close()
+    output.close()
 
 
 def load_data(filename):
 	
-	try:
-		pkl_file = open(filename, 'rb')
-	except Exception as e:
-		raise e
+    try:
+        pkl_file = open(filename, 'rb')
+    except Exception as e:
+        raise e
 
-	data = pickle.load(pkl_file)
+    data = pickle.load(pkl_file)
 
-	pkl_file.close()
+    pkl_file.close()
 
-	return data
+    return data
+
+
+def load_tf_check_point(session, filename):
+
+    saver = tf.train.Saver()
+
+    saver.restore(session, filename)
+
+    print("Model restored.")
+
+
+def save_tf_check_point(session, filename):
+    
+    saver = tf.train.Saver()
+
+    save_path = saver.save(session, filename)
+
+    print("tf checkpoint saved in file: %s" % save_path)
