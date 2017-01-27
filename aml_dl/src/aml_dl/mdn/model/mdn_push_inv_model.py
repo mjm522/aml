ecf833c96b4aa2d8f97636b152cc0b52c9db4400
Ermano Arruda <exa371@bham.ac.uk>
2017-01-27 14:30:13 +0000

    adding new sampling strategy

--- aml_dl/src/aml_dl/mdn/model/mdn_push_inv_model.py
+++ aml_dl/src/aml_dl/mdn/model/mdn_push_inv_model.py
@@ -17,7 +17,8 @@ class MDNPushInverseModel(object):
         self._device = self._params['device']
 
         with tf.device(self._device):
-            self._net_ops = tf_pushing_model(dim_input= network_params['dim_input'], 
+            self._net_ops = tf_pushing_model(dim_input= network_params['dim_input'],
+                               dim_output = network_params['dim_output'],
                                n_hidden = network_params['n_hidden'], 
                                n_kernels = network_params['k_mixtures'])
 
@@ -68,6 +69,16 @@ class MDNPushInverseModel(object):
 
             return samples
 
+    def sample_out_max_pi(self, x_input, m_samples = 10):
+
+        with tf.device(self._device):
+            out_pi, out_mu, out_sigma = self._sess.run([self._net_ops['pi'], self._net_ops['mu'], self._net_ops['sigma']], feed_dict={self._net_ops['x']: x_input})
+
+
+            samples = self._generate_mixture_samples_from_max_pi(out_pi, out_mu, out_sigma,m_samples)
+
+            return samples
+
     def expected_out(self, x_input, m_samples = 10):
         with tf.device(self._device):
             samples = self.sample_out(x_input,m_samples)[0]
@@ -78,6 +89,28 @@ class MDNPushInverseModel(object):
     def expected_out2(self, x_input, m_samples = 10):
         with tf.device(self._device):
             samples = self.sample_out(x_input,m_samples)[0]
+
+            out = np.zeros(2)
+
+            for i in range(m_samples):
+                out += np.array([np.cos(samples[i]),np.sin(samples[i])])
+            
+            out /= m_samples
+
+            out /= np.linalg.norm(out)
+
+            return out
+
+    def expected_max_pi_out(self, x_input, m_samples = 10):
+        with tf.device(self._device):
+            samples = self.sample_out_max_pi(x_input,m_samples)[0]
+
+            return np.mean(samples)
+
+
+    def expected_max_pi_out2(self, x_input, m_samples = 10):
+        with tf.device(self._device):
+            samples = self.sample_out_max_pi(x_input,m_samples)[0]
 
             out = np.zeros(2)
 
@@ -110,9 +143,36 @@ class MDNPushInverseModel(object):
         print 'failed to sample mixture weight index'
         return -1
 
+    def _max_pi_idx(self, pdf):
+        
+        i = np.argmax(pdf)
+
+        return i
+
     def _sample_gaussian(self, rn, mu, std):
 
         return mu + rn*std
+
+
+    def _generate_mixture_samples_from_max_pi(self, out_pi, out_mu, out_sigma, m_samples=10):
+
+        # Number of test inputs
+        N = out_mu.shape[0]
+
+        M = m_samples
+
+        result = np.random.rand(N, M)
+        rn  = np.random.randn(N, M) # normal random matrix (0.0, 1.0)
+
+        # Generates M samples from the mixture for each test input
+        for j in range(M):
+            for i in range(0, N):
+              idx = self._max_pi_idx(out_pi[i])
+              mu = out_mu[i, idx]
+              std = out_sigma[i, idx]
+              result[i, j] = self._sample_gaussian(rn[i, j], mu, std)
+
+        return result
 
 
     def _generate_mixture_samples(self, out_pi, out_mu, out_sigma, m_samples=10):
