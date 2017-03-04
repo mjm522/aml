7e2633a854cf478fe53e8d19bbfd387adf922b51
Ermano Arruda <ermano.arruda@gmail.com>
2017-03-04 20:32:04 +0000

    working on traingin mdn

--- aml_dl/src/aml_dl/mdn/tests/test_push_inv_model.py
+++ aml_dl/src/aml_dl/mdn/tests/test_push_inv_model.py
@@ -16,11 +16,11 @@ def get_data(operation):
 
     data_man = DataManager(data_folder_path=network_params_inv['training_data_path'], data_name_prefix='test_push_data')
 
-    # ids=range(0,5)
-    # x_keys = ['box_pos', 'box_ori', 'task_action']
-    # y_keys = ['box_pos', 'box_ori']
-    # x_sub_keys = [[None],[None],['push_xz']]
-    # y_sub_keys = [[None],[None]]
+    ids=range(0,5)
+    x_keys = ['box_pos', 'box_ori', 'task_action']
+    y_keys = ['box_pos', 'box_ori']
+    x_sub_keys = [[None],[None],['push_xz']]
+    y_sub_keys = [[None],[None]]
 
     # data_x, data_y = data_man.pack_data_in_range_xy(x_keys=x_keys, y_keys=y_keys, 
     #                                                 x_sub_keys=x_sub_keys, y_sub_keys=y_sub_keys, 
