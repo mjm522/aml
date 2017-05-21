49f2596429b769f691aaa0aad0827593471941a3
mjm522 <mjm522@cs.bham.ac.uk>
2017-05-21 11:09:35 +0100

    working satisfactorily for all three functions

--- aml_dl/src/aml_dl/mdn/model/tf_ensemble_mdn_model.py
+++ aml_dl/src/aml_dl/mdn/model/tf_ensemble_mdn_model.py
@@ -69,7 +69,7 @@ class EnsambleMDN(object):
 
     def train(self, x_train , y_train , sess, iterations):
         #training session
-        sess.run(tf.global_variables_initializer())
+        # sess.run(tf.global_variables_initializer())
 
         with tf.device(self._device_id):
             # Keeping track of loss progress as we train
@@ -86,10 +86,14 @@ class EnsambleMDN(object):
                     loss_grad = sess.run(grad_op,feed_dict={self._mdn_ensembles[k]._ops['x']: x_train, self._mdn_ensembles[k]._ops['y']: y_train})
                     
                     #get adversarial examples
-                    x_adv, y_adv = self.get_adversarial_examples(data_x = x_train, data_y = y_train, epsilon=0.0000001, loss_grad=loss_grad, no_examples=1)
+                    x_adv, y_adv = self.get_adversarial_examples(data_x = x_train, 
+                                                                 data_y = y_train, 
+                                                                 epsilon=0.01, 
+                                                                 loss_grad=loss_grad, 
+                                                                 no_examples=5)
                     
-                    x_train = copy.deepcopy(np.append(x_train, x_adv, axis=0))
-                    y_train = copy.deepcopy(np.append(y_train, y_adv, axis=0))
+                    x_train = copy.deepcopy(np.append(x_adv, x_train, axis=0))
+                    y_train = copy.deepcopy(np.append(y_adv, y_train, axis=0))
 
                     _, loss[k,i] = sess.run([train_op, loss_op], feed_dict={self._mdn_ensembles[k]._ops['x']: x_train, self._mdn_ensembles[k]._ops['y']: y_train})
 
