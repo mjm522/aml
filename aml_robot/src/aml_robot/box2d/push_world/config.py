5f908d0ed6c3a271fe42163e16122abbbb32abcc
Ermano Arruda <exa371@bham.ac.uk>
2017-05-31 01:20:57 +0100

    config

--- aml_robot/src/aml_robot/box2d/push_world/config.py
+++ aml_robot/src/aml_robot/box2d/push_world/config.py
@@ -15,14 +15,14 @@ config = {
 	'image_width': 640,
 	'image_height': 480,
 	'pixels_per_meter': 20.0,
-	'no_samples':2000,
-	'fps': 60,
+	'no_samples':100,
+	'fps': 30,
 	'dt': 0.0167,
 	'window_caption': 'BoxWorld',
 	'box_dim': (2,1),
 	'record_training_data': True,
 	'training_data_file': data_folder_path+'/data_test.pkl',
-	'steps_per_frame': 5,
+	'steps_per_frame': 15,
 	'data_folder_path':data_folder_path,
 
 }
