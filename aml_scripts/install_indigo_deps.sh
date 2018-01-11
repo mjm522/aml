f72b84bfb9052a0b15c663712c12cd41dc6c475b
Ermano Arruda <exa371@bham.ac.uk>
2018-01-11 17:45:45 +0000

    package source list script line for indigo

--- aml_scripts/install_indigo_deps.sh
+++ aml_scripts/install_indigo_deps.sh
@@ -12,7 +12,7 @@
 sudo apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 421C365BD9FF1F717815A3895523BAEEB01FA116
 
 # setup sources.list
-sudo echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list
+sudo sh -c "echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list"
 
 # installing ros-indigo-desktop-full
 sudo apt-get update 
