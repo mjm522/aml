77327e50f951bd80b053df8ef830ba18a422bb14
Ermano Arruda <ermano.arruda@gmail.com>
2018-02-21 16:50:00 +0000

    increasing sampling rate

--- aml_demos/src/aml_demos/config.py
+++ aml_demos/src/aml_demos/config.py
@@ -11,5 +11,5 @@ collect_robot_data_config = {
     'data_folder_path': data_storage_path,
     'num_samples_per_file':5,
     'data_name_prefix':'robot_data_collection',
-    'sampling_rate':30,
+    'sampling_rate':100,
 }
