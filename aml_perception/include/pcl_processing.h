5880befd1e3bf39144928fe25f266e48f6c56166
justagist <saifksidhik@gmail.com>
2018-03-09 17:20:42 +0000

    removed functions causing pcl to segfault

--- aml_perception/include/pcl_processing.h
+++ aml_perception/include/pcl_processing.h
@@ -1,28 +1,19 @@
 #include <pcl/point_cloud.h>  
 #include <pcl/point_types.h>  
 
-#include <pcl/io/pcd_io.h>
-#include <pcl/filters/extract_indices.h>
-#include <pcl/segmentation/sac_segmentation.h>
+// #include <pcl/filters/extract_indices.h>
+// #include <pcl/segmentation/sac_segmentation.h>
+// #include <pcl/filters/voxel_grid.h>
+// #include <pcl/filters/random_sample.h>
 
-#include <pcl/filters/voxel_grid.h>
-#include <pcl/filters/random_sample.h>
-
-#include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/transforms.h>
 #include <pcl_ros/point_cloud.h>
 
 #include <pcl/features/integral_image_normal.h>
 #include <pcl/features/normal_3d.h>
-#include <pcl/common/transforms.h>
-#include <pcl/io/ply_io.h>
-#include <pcl/io/vtk_io.h>
-#include <pcl/kdtree/kdtree_flann.h>
-#include <pcl/surface/poisson.h>
-#include <pcl/surface/gp3.h>
-#include <pcl/surface/marching_cubes_hoppe.h>
 
+#include <pcl/io/pcd_io.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/PointCloud.h>
 #include <sensor_msgs/point_cloud_conversion.h>
@@ -55,36 +46,11 @@ namespace aml_pcloud
      */
         PointCloudPtr pclCloudFromROSMsg(const sensor_msgs::PointCloud2 msg);
 
-    // /**
-    //  * Helper function to convert from sensor_msgs/PointCloud2 to pcl::PointCloud<pcl::PointXYZRGBNormal>
-    //  */
-        // pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr pclCloudRGBNormalFromROSMsg(const sensor_msgs::PointCloud2::ConstPtr& msg);
-
     /**
      *----- Helper function to convert from pcl::PointCloud<pcl::PointXYZ> to sensor_msgs/PointCloud2
      */
         sensor_msgs::PointCloud2::Ptr ROSMsgFromPclCloud(PointCloud& cloud);
-
-    // /**
-    //  * Helper function to convert from pcl::PointCloud<pcl::PointNormal> to sensor_msgs/PointCloud2
-    //  */
-    // sensor_msgs::PointCloud2::Ptr toROSMsg(pcl::PointCloud<pcl::PointNormal>& cloud)
-    // {
-    //     sensor_msgs::PointCloud2::Ptr msg(new sensor_msgs::PointCloud2);
-    //     pcl::toROSMsg(cloud, *msg);
-    //     return msg;
-    // };
-
-    // /**
-    //  * Helper function to convert from pcl::PointCloud<pcl::PointXYZRGBNormal> to sensor_msgs/PointCloud2
-    //  */
-    // sensor_msgs::PointCloud2::Ptr toROSMsg(pcl::PointCloud<pcl::PointXYZRGBNormal>& cloud)
-    // {
-    //     sensor_msgs::PointCloud2::Ptr msg(new sensor_msgs::PointCloud2);
-    //     pcl::toROSMsg(cloud, *msg);
-    //     return msg;
-    // };
-    
+  
 
     };
 
@@ -94,7 +60,7 @@ namespace aml_pcloud
     public:
 
         typedef std::shared_ptr<PCLProcessor> Ptr;
-        
+
         PCLProcessor() {}
 
         // ----- service request.function =  "read_pcd_file"
@@ -103,9 +69,9 @@ namespace aml_pcloud
         // ----- service request.function =  "save_to_file"
         void saveToPcdFile(std::string filename, const PointCloudPtr cloud);
 
-        pcl::PCLPointCloud2::Ptr downsamplePcdFile(const pcl::PCLPointCloud2::Ptr cloud);
+        // pcl::PCLPointCloud2::Ptr downsamplePcdFile(const pcl::PCLPointCloud2::Ptr cloud); // === CAUSES SEGFAULT !!
 
-        PointCloudPtr getPointsNotInPlane(PointCloudPtr input_cloud);
+        // PointCloudPtr getPointsNotInPlane(PointCloudPtr input_cloud); // === CAUSES SEGFAULT !!
 
         /**
          *  computeNormals function
@@ -130,13 +96,13 @@ namespace aml_pcloud
          */
         PointCloudPtr transformPointCloud(PointCloudPtr input_cloud, Eigen::Matrix4f camera_pose);
 
-        /**
+        /*
          *  Helper function to concatenate point clouds, with help from TA via Github
          *  @see https://github.com/Tomaat/CV2/issues/1
          *
          *  @param  cloud_base  the cloud to which the other cloud will be appended
          *  @param  cloud_add   the cloud to add to the other cloud
-         */
+        */
         void addPointCloud(PointCloudPtr cloud_base, PointCloudPtr cloud_add);
 
         /**provide an array of 3-D points (in columns), and this function will use and eigen-vector approach to find the best-fit plane
