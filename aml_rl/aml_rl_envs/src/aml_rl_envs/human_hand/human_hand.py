d8f93100bb05b5f001c262812a9cb5202da59b54
mjm522 <mjm522@cs.bham.ac.uk>
2018-03-26 13:51:07 +0100

    added human hand environment

---
+++ aml_rl/aml_rl_envs/src/aml_rl_envs/human_hand/human_hand.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pybullet as pb
+from os.path import join
+from aml_rl_envs.aml_rl_hand import AMLRlHand
+from aml_rl_envs.config import AML_RL_ROBOT_CONFIG
+from aml_rl_envs.bullet_visualizer import setup_bullet_visualizer
+
+class HumanHand(AMLRlHand):
+
+    def __init__(self, config=AML_RL_ROBOT_CONFIG, scale=1., 
+                       hand_type='right', use_fixed_base=False, 
+                       pos=(0.,0.,0.), ori=(0.,0.,0.,1.), j_pos=None, call_renderer=False):
+
+        config['call_renderer'] = call_renderer
+
+        self._config = config
+
+        self._ee_indices = []
+
+        self._hand_type = hand_type
+
+        self._scale = scale
+
+        self._use_fixed_base = use_fixed_base
+
+        self._base_pos = pos
+
+        self._base_ori = ori
+
+        self._defualt_jnts = j_pos
+
+        AMLRlHand.__init__(self, config, num_fingers=5)
+
+        self.reset()
+ 
+    def reset(self):
+
+        if self._hand_type == 'left':
+
+            urdf_file  = join(self._config['urdf_root_path'], "dlr_hand/dlr_hit2_left.urdf")
+            raise NotImplementedError("No urdf is present")
+
+        elif self._hand_type == 'right':
+
+            urdf_file  = join(self._config['urdf_root_path'], "human_hand/right_hand.urdf")
+
+        else:
+
+            raise Exception("Unknown hand type")
+
+        self._robot_id = pb.loadURDF(urdf_file, globalScaling=self._scale, useFixedBase=self._use_fixed_base)
+
+        self._finger_jnt_indices = [[1,2,3,4], #thumb finger joints
+                                    [5,6,7,8,9], # index finger joints
+                                    [10,11,12,13], # middle finger joints
+                                    [14,15,16,17], # ring finger joints
+                                    [18,19,20] ] # little finger joints
+
+        self.set_base_pose(pos=self._base_pos, ori=self._base_ori)
+
+        self.setup_hand()
---
+++ aml_rl/aml_rl_envs/src/aml_rl_envs/human_hand/human_hand_obj_env.py
@@ -0,0 +1,75 @@
+import os
+import copy
+import random
+import numpy as np
+import pybullet as pb
+from os.path import exists, join
+from aml_rl_envs.aml_rl_env import AMLRlEnv
+from aml_rl_envs.task.man_object import ManObject
+from aml_rl_envs.config import AML_RL_ROBOT_CONFIG
+from aml_rl_envs.human_hand.human_hand import HumanHand
+from aml_rl_envs.hand.config import HAND_OBJ_CONFIG, HAND_CONFIG
+
+class HumanHandObjEnv(AMLRlEnv):
+
+    def __init__(self,  action_dim,  demo2follow=None, 
+                        action_high=None, action_low=None, 
+                        randomize_box_ori=True, keep_obj_fixed = True, 
+                        config=HAND_OBJ_CONFIG):
+
+        self._goal_block_pos = np.array([0, 0, 0.75]) #x,y,z
+        
+        self._goal_obj_ori = np.array([0.0, 0.00, -1.158])
+
+        self._randomize_box_ori = randomize_box_ori
+
+        self._demo2follow = demo2follow
+
+        self._config = config
+
+        AMLRlEnv.__init__(self, config, set_gravity=True)
+
+        self._reset(obj_base_fixed = keep_obj_fixed)
+
+        obs_dim = 50
+
+        self.set_space_lims(obs_dim, action_dim, action_high, action_low)
+
+        self._seed()
+
+        pb.configureDebugVisualizer(pb.COV_ENABLE_RENDERING, 1)
+
+
+    def _reset(self, box_pos=[0., 0.1, 2.1], obj_base_fixed = True):
+
+        self.setup_env()
+
+        if self._randomize_box_ori:
+            
+            box_ori = pb.getQuaternionFromEuler([0., 0., np.random.uniform(-0.08*np.pi, 0.08*np.pi)])
+        
+        else:
+            
+            box_ori = [0.,0.,0.,1]
+
+        self._world_id = pb.loadURDF(join(self._urdf_root_path,"plane.urdf"))
+        
+        pb.resetBasePositionAndOrientation(self._world_id, [0., 0., -0.1], [0.,0.,0.,1])
+        
+        self._object = ManObject(urdf_root_path=self._config['urdf_root_path'], time_step=self._config['time_step'], 
+                                  pos=box_pos, ori=box_ori, scale=0.3, 
+                                  use_fixed_Base = obj_base_fixed, obj_type='cube')
+        
+        base_hand_pos  = [0., 0., 0.7]
+        
+        base_hand_ori  = pb.getQuaternionFromEuler([3*np.pi/2, 0., 0.])
+
+        self._hand = HumanHand(config=HAND_CONFIG, pos=base_hand_pos, ori=base_hand_ori, scale=3., use_fixed_base=True)
+
+        self._num_fingers = self._hand._num_fingers
+        
+        self._env_step_counter = 0
+        
+        pb.stepSimulation()
+      
+        return np.array(self._observation)
