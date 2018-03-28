9c62b1230b77fed17bc96ab856d573a69a3fa109
mjm522 <mjm522@cs.bham.ac.uk>
2018-03-28 22:47:36 +0100

    modified aml_logger to accept arbitrary arguments to format

--- aml_io/src/aml_io/log_utils.py
+++ aml_io/src/aml_io/log_utils.py
@@ -35,20 +35,25 @@ class AMLLogger(object):
 
         self._logger.setLevel(level)
 
-    def debug(self, msg):
-        self._logger.debug(msg)
+    def debug(self, msg, *args):
+        if args: msg += ' {}'
+        self._logger.debug(msg.format(args))
 
-    def info(self, msg):
-        self._logger.info(msg)
+    def info(self, msg, *args):
+        if args: msg += ' {}'
+        self._logger.info(msg.format(args))
 
-    def warning(self, msg):
-        self._logger.warning(msg)
+    def warning(self, msg, *args):
+        if args: msg += ' {}'
+        self._logger.warning(msg.format(args))
 
-    def error(self, msg):
-        self._logger.error(msg)
+    def error(self, msg, *args):
+        if args: msg += ' {}'
+        self._logger.error(msg.format(args))
 
-    def critical(self, msg):
-        self._logger.critical(msg)
+    def critical(self, msg, *args):
+        if args: msg += ' {}'
+        self._logger.critical(msg.format(args))
 
     def setLevel(self, level_name):
         level = LEVELS.get(level_name, logging.NOTSET)
--- aml_io/src/aml_io/tests/test_log_utils.py
+++ aml_io/src/aml_io/tests/test_log_utils.py
@@ -1,16 +1,47 @@
 import argparse
+import numpy as np
 from aml_io.log_utils import aml_logging
 
+class TestLogger():
+
+    def __init__(self):
+
+        self._logger = aml_logging.get_logger(__name__)
+
+        self._logger.debug("This is a debug message with array arguments:", np.random.randn(3), np.random.randn(3))
+        self._logger.debug("This is a debug message with integer argument:", 123)
+        self._logger.debug("This is a debug message with no arguments")
+
+        self._logger.info("This is a info message with array arguments:", np.random.randn(3))
+        self._logger.info("This is a info message with integer argument:", 123)
+        self._logger.info("This is a info message with no arguments")
+
+        self._logger.warning("This is a warning message with array arguments:", np.random.randn(3))
+        self._logger.warning("This is a warning message with integer argument:", 123)
+        self._logger.warning("This is a warning message with no arguments")
+
+        self._logger.error("This is a error message with array arguments:", np.random.randn(3))
+        self._logger.error("This is a error message with integer argument:", 123)
+        self._logger.error("This is a error message with no arguments")
+
+        self._logger.critical("This is a critical message with array arguments:", np.random.randn(3))
+        self._logger.critical("This is a critical message with integer argument:", 123)
+        self._logger.critical("This is a critical message with no arguments")
+
+
 def main():
-    parser = argparse.ArgumentParser(description='Train and test forward model')
-    parser.add_argument('-l', '--logtype', type=str, help='-l debug -l info, -l warning, -l critical, -l error')
-    args = parser.parse_args()
-    aml_logging.setup(args.logtype)
-    aml_logging.debug('This is a debug message')
-    aml_logging.info('This is an info message')
-    aml_logging.warning('This is a warning message')
-    aml_logging.error('This is an error message')
-    aml_logging.critical('This is a critical error message')
+
+    # parser = argparse.ArgumentParser(description='Train and test forward model')
+    # parser.add_argument('-l', '--logtype', type=str, default='debug' help='-l debug -l info, -l warning, -l critical, -l error')
+    # args = parser.parse_args()
+    # aml_logging.setup(args.logtype)
+    # aml_logging.debug('This is a debug message')
+    # aml_logging.info('This is an info message')
+    # aml_logging.warning('This is a warning message')
+    # aml_logging.error('This is an error message')
+    # aml_logging.critical('This is a critical error message')
+
+    tl = TestLogger()
 
 
 if __name__ == '__main__':
