a454f92d5226449a3d697d6967bc4baef074c907
mjm522 <mjm522@cs.bham.ac.uk>
2018-04-11 16:44:12 +0100

    bug correction in js position control

--- aml_ctrl/src/aml_ctrl/controllers/js_controllers/js_postn_controller.py
+++ aml_ctrl/src/aml_ctrl/controllers/js_controllers/js_postn_controller.py
@@ -42,7 +42,7 @@ class JSPositionController(JSController):
 
             goal_js_acc       = self._goal_js_acc
 
-        robot_state    = self.state()
+        robot_state    = self._robot.state()
 
         q              = robot_state['position']
 
