90dbf5b317fc956075d82e053c29cc057218a899
mjm522 <mjm522@cs.bham.ac.uk>
2018-04-12 15:33:14 +0100

    seperate client id tested on dlr hand env

--- aml_rl/aml_rl_envs/src/aml_rl_envs/dlr_hand/dlr_hand_obj_env.py
+++ aml_rl/aml_rl_envs/src/aml_rl_envs/dlr_hand/dlr_hand_obj_env.py
@@ -37,7 +37,7 @@ class DLRHandObjEnv(AMLRlEnv):
 
         self._seed()
 
-        pb.configureDebugVisualizer(pb.COV_ENABLE_RENDERING, 1)
+        pb.configureDebugVisualizer(pb.COV_ENABLE_RENDERING, 1, physicsClientId=self._cid)
 
 
     def _reset(self, box_pos=[-0.4, 0., 1.1], obj_base_fixed = True):
@@ -46,30 +46,30 @@ class DLRHandObjEnv(AMLRlEnv):
 
         if self._randomize_box_ori:
             
-            box_ori = pb.getQuaternionFromEuler([0., 0., np.random.uniform(-0.08*np.pi, 0.08*np.pi)])
+            box_ori = pb.getQuaternionFromEuler([0., 0., np.random.uniform(-0.08*np.pi, 0.08*np.pi)], physicsClientId=self._cid)
         
         else:
             
             box_ori = [0.,0.,0.,1]
 
-        self._world_id = pb.loadURDF(join(self._urdf_root_path,"plane.urdf"))
+        self._world_id = pb.loadURDF(join(self._urdf_root_path,"plane.urdf"), physicsClientId=self._cid)
         
-        pb.resetBasePositionAndOrientation(self._world_id, [0., 0., -0.1], [0.,0.,0.,1])
+        pb.resetBasePositionAndOrientation(self._world_id, [0., 0., -0.1], [0.,0.,0.,1], physicsClientId=self._cid)
         
-        self._object = ManObject(urdf_root_path=self._config['urdf_root_path'], time_step=self._config['time_step'], 
+        self._object = ManObject(cid=self._cid, urdf_root_path=self._config['urdf_root_path'], time_step=self._config['time_step'], 
                                   pos=box_pos, ori=box_ori, scale=0.3, 
                                   use_fixed_Base = obj_base_fixed, obj_type='cube')
         
         base_hand_pos  = [0., 0., 0.7]
         
-        base_hand_ori  = pb.getQuaternionFromEuler([0., 3*np.pi/2, 0.])
+        base_hand_ori  = pb.getQuaternionFromEuler([0., 3*np.pi/2, 0.], physicsClientId=self._cid)
 
-        self._hand = DLRHand(config=HAND_CONFIG, pos=base_hand_pos, ori=base_hand_ori, scale=3., use_fixed_base=True)
+        self._hand = DLRHand(cid=self._cid, config=HAND_CONFIG, pos=base_hand_pos, ori=base_hand_ori, scale=3., use_fixed_base=True)
 
         self._num_fingers = self._hand._num_fingers
         
         self._env_step_counter = 0
         
-        pb.stepSimulation()
+        pb.stepSimulation(physicsClientId=self._cid)
       
         return np.array(self._observation)
