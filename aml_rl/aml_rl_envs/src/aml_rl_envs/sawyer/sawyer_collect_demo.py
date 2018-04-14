095da9e76f8710e6250ca26e62c4314e399177e1
mjm522 <mjm522@cs.bham.ac.uk>
2018-04-14 04:11:58 +0100

    keyboard control for bullet

--- aml_rl/aml_rl_envs/src/aml_rl_envs/sawyer/sawyer_collect_demo.py
+++ aml_rl/aml_rl_envs/src/aml_rl_envs/sawyer/sawyer_collect_demo.py
@@ -1,18 +1,30 @@
 import os
-from aml_rl_envs.sawyer.sawyer_env import SawyerEnv
+from aml_rl_envs.sawyer.sawyer_peg_env import SawyerEnv
+from aml_rl_envs.utils.keyboard_control import KeyBoardControl
 from aml_rl_envs.utils.manual_control_collect_demo import ManualDemoCollect
 from aml_rl_envs.utils.collect_demo import CollectDemo, get_demo
 
 collect_demo = True
 
-def main():
 
-    env = SawyerEnv(renders=True, isDiscrete=False, maxSteps = 10000000)
+def demo_collect(env):
 
-    cd = ManualDemoCollect(manipulator=env._sawyer, demo_path=os.environ["ROOT_DIR"]+'/data/sawyerDemo')
-    
+    cd = ManualDemoCollect(manipulator=env._sawyer, demo_path=os.environ["AML_DATA"]+'/data/sawyerDemo')
     cd.collect_demo()
     cd.check_demo()
+
+def keyboard_ctrl(env):
+
+    kc = KeyBoardControl(manipulator=env._sawyer)
+    kc.run()
+
+
+def main():
+
+    env = SawyerEnv()
+    keyboard_ctrl(env)
+
+
 
 
 if __name__ == '__main__':
