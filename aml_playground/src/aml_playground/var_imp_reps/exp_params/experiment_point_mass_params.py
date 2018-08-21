0279dcae6c2d0228114f6ba3cff233733a1609c6
justagist <saifksidhik@gmail.com>
2018-08-21 10:02:37 +0100

    change in point mass env to take induvidual policy per time step of one single policy for whole task

--- aml_playground/src/aml_playground/var_imp_reps/exp_params/experiment_point_mass_params.py
+++ aml_playground/src/aml_playground/var_imp_reps/exp_params/experiment_point_mass_params.py
@@ -20,7 +20,8 @@ gpreps_params = {
     'num_fake_data':30,
     'num_samples_per_update':30,
     'num_samples_fwd_data':50,
-    'w_bounds': np.array([[-0.015, -0.013, 0., 0.],[ 0.015,  0.013, 0., 0.05]])
+    'w_bounds': np.array([[-0.015, -0.013, 0., 0.],[ 0.015,  0.013, 0., 0.05]]),
+    'policy_per_time_step':True
 }
 
 
@@ -136,4 +137,26 @@ experiment_8['env_params']['force_predict_model'] = NextForcePredictModel(spring
 experiment_8['start_policy'] = None#os.environ['AML_DATA'] + '/aml_playground/imp_worlds/point_mass/creps_data_point_mass_spring_stiif5_6.pkl'
 experiment_8['param_file_name']=os.environ['AML_DATA'] + '/aml_playground/imp_worlds/point_mass/creps_data_point_mass_exp_7_6_with_all_equal_weighting.pkl'
 
-exp_params = experiment_8
+experiment_9 =  copy.deepcopy(experiment_1)
+kd_scale = 50.
+kp_scale = (kd_scale**2.)/4.
+experiment_9['n_episodes']=1000
+experiment_9['smooth_policy']=False
+experiment_9['env_params']['spring_stiffness'] = 3.0
+experiment_9['env_params']['reward_gamma'] = 1.#0.99
+experiment_9['env_params']['ramp_traj_flag'] = False
+experiment_9['env_params']['delta_u_weight'] = 0.25
+experiment_9['env_params']['u_weight'] = 0.25
+experiment_9['env_params']['goal_weight'] = 0.25
+experiment_9['env_params']['finishing_weight'] = 0.25
+experiment_9['gpreps_params']['entropy_bound'] = 2.
+experiment_9['gpreps_params']['context_dim'] = 9
+experiment_9['gpreps_params']['context_feature_dim'] = 9
+experiment_9['gpreps_params']['policy_per_time_step'] = False
+experiment_9['env_params']['enable_sigmoid'] = False
+experiment_9['env_params']['param_scale'] = np.array([ kp_scale, kp_scale, kp_scale, kd_scale,  kd_scale,  kd_scale ])
+experiment_9['env_params']['force_predict_model'] = NextForcePredictModel(spring_k=experiment_9['env_params']['spring_stiffness'])
+experiment_9['start_policy'] = None#os.environ['AML_DATA'] + '/aml_playground/imp_worlds/point_mass/creps_data_point_mass_spring_stiif5_6.pkl'
+experiment_9['param_file_name']=os.environ['AML_DATA'] + '/aml_playground/imp_worlds/point_mass/creps_data_point_mass_exp_single_policy.pkl'
+
+exp_params = experiment_9
