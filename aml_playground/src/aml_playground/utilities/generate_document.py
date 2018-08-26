96390f432d97e7b2e83e47e900b3476348556a7e
Ermano Arruda <exa371@bham.ac.uk>
2018-08-26 22:17:34 +0100

    generate experiment document

---
+++ aml_playground/src/aml_playground/utilities/generate_document.py
@@ -0,0 +1,149 @@
+import os
+import numpy as np
+from pylatex.utils import italic, NoEscape
+from pylatex import Document, Math, Section, Subsection, Tabular, Tabularx, Command, Figure, SubFigure, NoEscape, Itemize, Alignat
+from aml_playground.var_imp_reps.exp_params.experiment_point_mass_params import exp_params
+
+class GenerateDocument():
+
+    def __init__(self, config):
+
+        geometry_options = {
+        "head": "10pt",
+        "margin": "0.2in",
+        "bottom": "0.2in",
+        "includeheadfoot": False
+        }
+        
+        self._doc = Document(config['file_name'], geometry_options=geometry_options)
+        self._doc.preamble.append(Command('title', config['title']))
+        self._doc.preamble.append(Command('author', config['authors']))
+        self._doc.preamble.append(Command('date', NoEscape(r'\today')))
+        self._doc.append(NoEscape(r'\maketitle'))
+
+    def add_image(self, image_filenames, image_captions):
+
+        for k in range(2):
+  
+            with self._doc.create(Figure(position='h!')) as image:
+
+                with self._doc.create(SubFigure(
+                        position='b',
+                        width=NoEscape(r'0.45\linewidth'))) as left_image:
+
+                    left_image.add_image(image_filenames[k][0],
+                                          width=NoEscape(r'\linewidth'))
+                    left_image.add_caption(image_captions[k][0])
+
+                with self._doc.create(SubFigure(
+                        position='b',
+                        width=NoEscape(r'0.45\linewidth'))) as right_image:
+
+                    right_image.add_image(image_filenames[k][1],
+                                           width=NoEscape(r'\linewidth'))
+                    right_image.add_caption(image_captions[k][1])
+
+
+    def add_table(self, table_values=None):
+
+        table_rows = ['Goal pos weight', 'Goal vel weight', 'Control weight','Delta Ctrl weight', 'Kp-Kd weight','Delta Kp-Kd weight',
+                    'Cumsum reward', 'Sigmoid reward','Gamma','Next force prediction','Time step','Kd Scale']
+
+
+        with self._doc.create(Subsection('Table of Hyperparameters')):
+
+            with self._doc.create(Tabular(table_spec='|l|c||',
+                                         row_height=1.4)) as table:
+
+                for k in range(len(table_rows)):
+                    table.add_hline()
+                    table.add_row((table_rows[k], table_values[k]))
+                    table.add_hline()
+
+
+    def fill_document(self):
+
+        with self._doc.create(Section('Point-Mass object pulling a spring (pybullet)')):
+            
+            self._doc.append('Task: Learn the varying 3D Kp and Kd required to pull a spring to a desired height (with minimum effort)')
+            with self._doc.create(Itemize()) as itemize:
+                itemize.add_item("100 time steps ( Hence, 100 x (3 + 3) parameters to learn as single policy or 100 separate policies )")
+                itemize.add_item("Learner: CREPS")
+                itemize.add_item("Policy: Linear Gaussian")
+
+            self._doc.append('Other Info:')
+            with self._doc.create(Itemize()) as itemize:
+                itemize.add_item("Time steps = 100")
+                itemize.add_item("Spring stiffness = 3")
+                itemize.add_item("Episodes = 1000")
+                itemize.add_item("Kp scale = 0.25*(Kd scale^2)")
+
+            self._doc.append('REPS params:')
+            with self._doc.create(Itemize()) as itemize:
+                itemize.add_item("Entropy bound = 2.0")
+                itemize.add_item("Context dim = 9 (3x pos, 3x delta_pos, 3x force)")
+                itemize.add_item("Context feature dim = 9")
+                itemize.add_item("1 policy per time step")
+
+            self._doc.append("Cost function")
+            with self._doc.create(Alignat(numbering=False, escape=False)) as agn:
+                agn.append(r'\sum_t \left( X^T_t Q_1 X_t + \dot{X}_t^T Q_2 \dot{X}_t + U^T_t R_1 U_t + \dot{U}_t^T R_2 \dot{U}_t + K^T_t R_3 K_t + \dot{K_t}^T R_4 \dot{K_t} \right)')
+
+    def finish(self, file_name=None):
+
+        if file_name is None:
+            file_name = './basic_pdf'
+
+        self._doc.generate_pdf(file_name, clean_tex=False)
+        tex = self._doc.dumps()
+        self._doc.generate_pdf(clean_tex=False)
+
+
+
+def main():
+    
+    config = {'file_name':'./basic',
+    'title':'Variable Impedance Learning',
+    'authors':'Mike and Saif'}
+
+    gen_doc = GenerateDocument(config)
+    gen_doc.fill_document()
+
+    base_path = os.environ['AML_DATA'] + '/aml_playground/imp_worlds/point_mass/'
+    
+    image_filenames = [ [base_path+'Kp-Kd-u.png', base_path+'Traj-Vel.png'],
+                      [base_path+'Reward.png', base_path+'Reward_traj.png',],
+                      ]
+    image_captions = [['Param values', 'Trajectory and Velocity'],['Mean reward', 'Reward splits']]
+
+    '''
+    ['Goal pos weight', 'Goal vel weight', 'Control weight','Delta Ctrl weight', 'Kp-Kd weight','Delta Kp-Kd weight',
+                    'Enable cumsum reward', 'Enable sigmoid reward','Gamma','Next force prediction','Time step','Kd Scale']
+    '''
+
+    if exp_params['env_params']['force_predict_model'] is not None:
+        force_predict_model = True
+
+    table_values=[exp_params['env_params']['goal_pos_weight'][0,0],
+                  exp_params['env_params']['goal_vel_weight'][0,0],
+                  exp_params['env_params']['u_weight'][0,0],
+                  exp_params['env_params']['delta_u_weight'][0,0],
+                  exp_params['env_params']['param_weight'][0,0],
+                  exp_params['env_params']['delta_param_weight'][0,0],
+                  exp_params['env_params']['enable_cumsum'],
+                  exp_params['env_params']['enable_sigmoid'],
+                  exp_params['env_params']['reward_gamma'],
+                  force_predict_model,
+                  exp_params['env_params']['time_step'],
+                  exp_params['env_params']['param_scale'][-1],
+                  ]
+
+    gen_doc.add_table(table_values=table_values)
+
+    gen_doc.add_image(image_filenames=image_filenames, image_captions=image_captions)
+
+    gen_doc.finish()
+
+
+if __name__ == '__main__':
+    main()
